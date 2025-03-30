e7519a67d4d0284cb0f40ed68aaf3c8121d599ad
Janpstrunn <janpstrunn@disroot.org>
2025-03-30 12:37:07 -0300

    hyprland: Add more packages

--- apps/wm/hyprland.nix
+++ apps/wm/hyprland.nix
@@ -1,22 +1,24 @@
 { config, pkgs, ... }:
 
-{ 
-  environment.sessionVariables.NIXOS_OZONE_WL = "1"; 
+{
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
   environment.systemPackages = with pkgs; [
       # Hyprland Essentials
       hyprland
       hyprlang
       meson
-      wlsunset 
-      wl-clipboard 
+      wlsunset
+      wl-clipboard
       waybar
       dunst
       cpio
       cliphist
       playerctl
-      xdg-desktop-portal 
+      xdg-desktop-portal
       xdg-desktop-portal-gtk
       xdg-desktop-portal-hyprland
+      xclip
+      wl-clipboard-x11
       # Extra Hyprland
       hyprpaper
       hypridle
@@ -26,7 +28,8 @@
       hyprsunset
       # Extra Tools
       swww
-      rofi-wayland 
+      rofi-wayland
+      fuzzel
       wttrbar
       swayimg
       grim
