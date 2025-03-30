a7b6788b1f9b9405aa5b55ce020bf7c7e6e895f1
Janpstrunn <janpstrunn@disroot.org>
2025-03-30 15:33:42 -0300

    fix: Proper syntax for home manager variables

--- flake.nix
+++ flake.nix
@@ -21,12 +21,10 @@ description = "Elegant Vagrant";
       };
       # ----- USER SETTINGS ----- #
       # This user is in wheel group
-      userSettings = {
-        username = "janpstrunn";
-        name = "Janpstrunn";
-        email = "janpstrunn@disroot.org";
-        homeDir = "/home/${userSettings.username}";
-      };
+      username = "janpstrunn";
+      name = "Janpstrunn";
+      email = "janpstrunn@disroot.org";
+      homeDir = "/home/${username}";
       lib = nixpkgs.lib;
       system = "x86_64-linux";
       pkgs = nixpkgs.legacyPackages."${system}";
@@ -35,15 +33,15 @@ description = "Elegant Vagrant";
           janpstrunn = lib.nixosSystem {
               system = "${system}";
               modules = [ ./configuration.nix ];
-              specialArgs = {
-                 inherit username;
-              };
             };
         };
       homeConfigurations = {
           janpstrunn = home-manager.lib.homeManagerConfiguration {
               inherit pkgs;
               modules = [ ./home.nix ];
+              extraSpecialArgs = {
+                 inherit username;
+              };
             };
         };
     };
