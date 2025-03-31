90509ca93f8499fc7e795def2332f5e1e9c1a61c
Janpstrunn <janpstrunn@disroot.org>
2025-03-31 14:53:34 -0300

    system: Adjust locale to use us time format

--- modules/system/locale.nix
+++ modules/system/locale.nix
@@ -3,6 +3,17 @@
 {
   time.timeZone = systemSettings.timezone;
   i18n.defaultLocale = systemSettings.locale;
+  # i18n.extraLocaleSettings = {
+  #   LC_ADDRESS = systemSettings.extra_locale;
+  #   LC_IDENTIFICATION = systemSettings.extra_locale;
+  #   LC_MEASUREMENT = systemSettings.extra_locale;
+  #   LC_MONETARY = systemSettings.extra_locale;
+  #   LC_NAME = systemSettings.extra_locale;
+  #   LC_NUMERIC = systemSettings.extra_locale;
+  #   LC_PAPER = systemSettings.extra_locale;
+  #   LC_TELEPHONE = systemSettings.extra_locale;
+  #   LC_TIME = systemSettings.extra_locale;
+  # };
   i18n.extraLocaleSettings = {
     LC_ADDRESS = systemSettings.extra_locale;
     LC_IDENTIFICATION = systemSettings.extra_locale;
@@ -12,7 +23,7 @@
     LC_NUMERIC = systemSettings.extra_locale;
     LC_PAPER = systemSettings.extra_locale;
     LC_TELEPHONE = systemSettings.extra_locale;
-    LC_TIME = systemSettings.extra_locale;
+    LC_TIME = systemSettings.locale;
   };
 }
 
