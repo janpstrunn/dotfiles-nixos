3545bdf1c23df67db66eabda01717b67cb216ca7
Janpstrunn <janpstrunn@disroot.org>
2025-04-05 14:12:21 -0300

    user: Set librewolf as default browser

--- profiles/desktop/home.nix
+++ profiles/desktop/home.nix
@@ -77,8 +77,16 @@
     };
   };
 
-  xdg.mime.enable = true;
-  xdg.mimeApps.enable = true;
+  xdg.mimeApps = {
+    enable = true;
+    defaultApplications = {
+      "text/html" = "librewolf.desktop";
+      "x-scheme-handler/http" = "librewolf.desktop";
+      "x-scheme-handler/https" = "librewolf.desktop";
+      "x-scheme-handler/about" = "librewolf.desktop";
+      "x-scheme-handler/unknown" = "librewolf.desktop";
+    };
+  };
 
   home.packages = with pkgs; [
     # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
