a64047eb0db9f1df2ce09e68cff869743f9fb0e6
Janpstrunn <janpstrunn@disroot.org>
2025-04-18 15:17:07 -0300

    chore(system): Allow unfree steam only

--- modules/system/steam.nix
+++ modules/system/steam.nix
@@ -1,7 +1,13 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 
 {
+  environment.systemPackages = with pkgs; [
+    steam
+  ];
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+    "steam"
+    "steam-unwrapped"
+  ];
   hardware.opengl.driSupport32Bit = true;
   programs.steam.enable = true;
-  environment.systemPackages = [ pkgs.steam ];
 }
