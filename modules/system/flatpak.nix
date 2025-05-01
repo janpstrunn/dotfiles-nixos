a8581a2053a34c4fbb73e0a80faec2bf5c7df64e
Janpstrunn <janpstrunn@disroot.org>
2025-05-01 16:21:33 -0300

    chore(system): Remove nix-flatpak options

--- modules/system/flatpak.nix
+++ modules/system/flatpak.nix
@@ -1,11 +1,5 @@
 {...}: {
   services.flatpak = {
     enable = true;
-    remotes = [
-      {
-        name = "flathub";
-        location = "https://flathub.org/repo/flathub.flatpakrepo";
-      }
-    ];
   };
 }
