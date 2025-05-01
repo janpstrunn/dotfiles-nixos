a10f06737c5e1bd52892301163e0cf44e59cb448
Janpstrunn <janpstrunn@disroot.org>
2025-05-01 19:57:59 -0300

    chore(user): Add laptop config

--- profiles/laptop/home.nix
+++ profiles/laptop/home.nix
@@ -1,1 +1,25 @@
-/home/janpstrunn/nix/profiles/desktop/home.nix
+{...}: {
+  imports = [
+    # Home Manager
+    ../../modules/home.nix
+    # Programming Languages
+    # ../../modules/user/lang/android.nix
+    # ../../modules/user/lang/cpp.nix
+    # ../../modules/user/lang/go.nix
+    ../../modules/user/lang/lsp.nix
+    # ../../modules/user/lang/nodejs.nix
+    # ../../modules/user/lang/python.nix
+    # ../../modules/user/lang/ruby.nix
+    # ../../modules/user/lang/rust.nix
+    # Applications
+    ../../modules/user/apps/browser.nix
+    ../../modules/user/apps/cli.nix
+    # ../../modules/user/apps/drive.nix
+    ../../modules/user/apps/editor.nix
+    ../../modules/user/apps/email.nix
+    ../../modules/user/apps/media.nix
+    ../../modules/user/apps/office.nix
+    ../../modules/user/apps/password-manager.nix
+    ../../modules/user/apps/terminal.nix
+  ];
+}
