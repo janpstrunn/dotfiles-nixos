cc657e2d086a0a1983ec7c6bf72ee1072f513da3
Janpstrunn <janpstrunn@disroot.org>
2025-05-03 12:38:51 -0300

    feat(user): Add gpg-agent config file

--- modules/user/files.nix
+++ modules/user/files.nix
@@ -6,5 +6,10 @@
       $DEV/github/
       $DEV/forgejo/
     '';
+    ".gnupg/gpg-agent.conf".text = ''
+      # pinentry-program /usr/bin/pinentry-gnome3
+      max-cache-ttl 60480000
+      default-cache-ttl 60480000
+    '';
   };
 }
