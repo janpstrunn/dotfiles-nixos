6ed5ccac12fac66c82df06b814073bd4c1fd512e
Janpstrunn <janpstrunn@disroot.org>
2025-05-13 19:38:01 -0300

    chore(user): Set lsp as extraPackages

--- modules/user/apps/nvim.nix
+++ modules/user/apps/nvim.nix
@@ -1,21 +1,27 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    alejandra
-    bash-language-server
-    clang-tools
-    # docker-compose-language-service
-    # dockerfile-language-server-nodejs
-    harper
-    # java-language-server
-    # kotlin-language-server
-    lua-language-server
-    markdown-oxide
-    marksman
     neovide
-    neovim
-    python311Packages.python-lsp-server
-    sqls
-    # typescript-language-server
-    yaml-language-server
   ];
+
+  programs.neovim = {
+    enable = true;
+    extraPackages = with pkgs; [
+      alejandra
+      bash-language-server
+      # clang-tools
+      # docker-compose-language-service
+      # dockerfile-language-server-nodejs
+      harper
+      # java-language-server
+      # kotlin-language-server
+      lua-language-server
+      # markdown-oxide
+      # marksman
+      neovide
+      python311Packages.python-lsp-server
+      # sqls
+      # typescript-language-server
+      # yaml-language-server
+    ];
+  };
 }
