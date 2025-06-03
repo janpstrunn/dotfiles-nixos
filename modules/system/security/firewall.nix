c242698f4c3566a43d2d6e38024db56cdec9d6ad
Janpstrunn <janpstrunn@disroot.org>
2025-06-03 13:56:53 -0300

    chore(system): Tidy firewall config

--- modules/system/security/firewall.nix
+++ modules/system/security/firewall.nix
@@ -1,9 +1,12 @@
 {...}: {
   # Firewall
-  networking.firewall.enable = true;
-  # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [8080];
-  networking.firewall.allowedUDPPorts = [8080];
+  networking.firewall = {
+    # Open ports in the firewall.
+    enable = true;
+    allowedTCPPorts = [8080];
+    allowedUDPPorts = [8080];
+    # interfaces."wlan0".allowedTCPPorts = [8080];
+  };
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
 }
