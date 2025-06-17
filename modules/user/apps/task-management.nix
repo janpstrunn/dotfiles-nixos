2271489cbd51ec824cef0c88cd2941bb0c248362
Janpstrunn <janpstrunn@disroot.org>
2025-06-17 15:27:25 -0300

    chore(user): Add restic and taskchampion

--- modules/user/apps/task-management.nix
+++ modules/user/apps/task-management.nix
@@ -2,5 +2,6 @@
   home.packages = with pkgs; [
     taskwarrior3
     taskwarrior-tui
+    taskchampion-sync-server
   ];
 }
--- modules/user/apps/task.nix
+++
@@ -1,5 +0,0 @@
-{pkgs, ...}: {
-  home.packages = with pkgs; [
-    taskchampion-sync-server
-  ];
-}
