1d1f4624e4da470a83da3b58131144baaa8d1064
Janpstrunn <janpstrunn@disroot.org>
2025-06-22 17:18:00 -0300

    chore(profile): Update desktop hardware

--- profiles/desktop/hardware-configuration.nix
+++ profiles/desktop/hardware-configuration.nix
@@ -8,25 +8,10 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "ehci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "ehci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
-
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/d08a5eff-7dac-4cce-b708-6fab42b77dbe";
-      fsType = "ext4";
-    };
-
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/BC6D-EA89";
-      fsType = "vfat";
-      options = [ "fmask=0077" "dmask=0077" ];
-    };
-
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/3bab60c2-b01e-4972-bcf4-19039160b820"; }
-    ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
